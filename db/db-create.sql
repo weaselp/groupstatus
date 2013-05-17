1c7ad89f53680918d71b1da6e45d37f19b8497e0
Peter Palfrader <peter@palfrader.org>
2013-05-17 21:21:20 +0200

    db schema

---
+++ db/db-create.sql
@@ -0,0 +1,36 @@
+
+CREATE TABLE config (
+    name        TEXT    PRIMARY KEY,
+    value       TEXT
+);
+INSERT INTO config VALUES ('db_revision', '1');
+GRANT SELECT ON config TO public;
+
+
+CREATE TABLE message (
+    message_id  SERIAL  PRIMARY KEY,
+    ts          TIMESTAMP NOT NULL,
+    channel     TEXT NOT NULL,
+    person      TEXT NOT NULL,
+    message     TEXT NOT NULL
+);
+CREATE INDEX message_ts_idx ON message(ts);
+CREATE INDEX message_channel_idx ON message(channel);
+CREATE INDEX message_person_idx ON message(person);
+GRANT SELECT ON message TO public;
+
+CREATE TABLE tag (
+    tag_id      SERIAL  PRIMARY KEY,
+    tag         TEXT NOT NULL
+);
+CREATE INDEX tag_tag_idx ON tag(tag);
+GRANT SELECT ON tag TO public;
+
+CREATE TABLE message_tag (
+    message_id INTEGER NOT NULL REFERENCES message(message_id),
+    tag_id     INTEGER NOT NULL REFERENCES tag(tag_id),
+    PRIMARY KEY (message_id, tag_id)
+);
+CREATE INDEX message_tag_message_id ON message_tag(message_id);
+CREATE INDEX message_tag_tag_id ON message_tag(tag_id);
+GRANT SELECT ON message_tag TO public;
